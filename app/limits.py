35d254f60b207a4b0a0bff24c1cceabf3e2ac035
Milkoon2916 <chongrojj43@gmail.com>
2026-09-01 10:34:39 +0900

    Delete app directory

--- app/limits.py
+++
@@ -1,6 +0,0 @@
-"""
-용량 제한값 한곳에 모아둠. 나중에 정책 바뀌면 여기만 고치면 됨.
-"""
-MAX_WORD_LISTS_PER_TEACHER = 100   # 단어장(폴더) 개수
-MAX_WORDS_PER_TEACHER = 5000       # 선생님 계정 전체 단어 개수 합
-MAX_STUDENTS_PER_TEACHER = 100     # 학생 등록 인원
